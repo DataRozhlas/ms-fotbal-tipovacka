aa61ced3ba8cfc3e1bc54bee7fccb5aa457db961
Petr Kočí <petr.koci@samizdat.cz>
2022-11-04 16:37:34 +0100

    basic layout

---
+++ components/SelectBox/index.tsx
@@ -0,0 +1,40 @@
+const countries = [
+  { name: "Katar", id: "qa" },
+  { name: "Brazílie", id: "br" },
+  { name: "Belgie", id: "be" },
+  { name: "Francie", id: "fr" },
+  { name: "Argentina", id: "ar" },
+  { name: "Anglie", id: "gb_eng" },
+  { name: "Španělsko", id: "es" },
+  { name: "Portugalsko", id: "pt" },
+  { name: "Mexiko", id: "mx" },
+  { name: "Nizozemsko", id: "nl" },
+  { name: "Dánsko", id: "dk" },
+  { name: "Německo", id: "de" },
+  { name: "Uruguay", id: "uy" },
+  { name: "Švýcarsko", id: "ch" },
+  { name: "USA", id: "us" },
+  { name: "Chorvatsko", id: "hr" },
+  { name: "Senegal", id: "sn" },
+  { name: "Írán", id: "ir" },
+  { name: "Japonsko", id: "jp" },
+  { name: "Maroko", id: "ma" },
+  { name: "Srbsko", id: "rs" },
+  { name: "Polsko", id: "pl" },
+  { name: "Jižní Korea", id: "kr" },
+  { name: "Tunisko", id: "tn" },
+  { name: "Kamerun", id: "cm" },
+  { name: "Kanada", id: "ca" },
+  { name: "Ekvádor", id: "ec" },
+  { name: "Saudská Arábie", id: "sa" },
+  { name: "Ghana", id: "gh" },
+  { name: "Wales", id: "gb-wls" },
+  { name: "Kostarika", id: "cr" },
+  { name: "Austrálie", id: "au" },
+];
+
+const SelectBox = () => {
+  return <div>SelectBox</div>;
+};
+
+export { SelectBox };
